49587cdb89e0baab98d86a5725d2bdb7dfc09a8d
taylorbohl <tbohl3@gmail.com>
2022-03-23 22:11:20 -0500

    adding to python script

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,8 +6,7 @@ import csv
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 
-#define empty lists for values and calculations below
-monthyear = []
+#empty list for profit changes to be stored
 month_to_month = []
 
 #read csv file
@@ -21,8 +20,7 @@ with open(csvpath) as csvfile:
     
     # grab first row of data
     first_row = next(csvreader)
-    #store date information in list
-    monthyear.append(first_row[0])
+ 
     # first profit value
     profit_initial = first_row[1]
 
@@ -31,28 +29,43 @@ with open(csvpath) as csvfile:
     net_total = int(profit_initial)
 
     for row in csvreader:
-        #store date information in list
-        monthyear.append(row[0])
+        #count months (rows) for total months
+        count += 1
+        net_total += int(row[1])
 
         #find profit change and store in list
         profit_change = int(row[1]) - int(profit_initial)
         month_to_month.append(profit_change)
 
-        #count months (rows) for total months
-        count += 1
-        net_total += int(row[1])
-
         #reset profit initial to current value for next loop
         profit_initial = row[1]
-        
 
-    #print data in table
-    print("Financial Analysis")
-    print("-----------------")
-    print("Total Months: " + str(count))
-    print("Total: $" + str(net_total))
-    print("Average Change: " + str(round((sum(month_to_month)/count),0)))
-    print("Greatest Increase in Profits: " + monthyear[79] + " ($" + str(max(month_to_month)) + ")")
-    #print(month_to_month.index(1862002))
-    print("Greatest Decrease in Profits: " + monthyear[49] + " ($" + str(min(month_to_month)) + ")")
-    #print(month_to_month.index(-1825558))
+        #define variables for table
+        greatest_month_increase = (max(month_to_month))
+        greatest_month_decrease = (min(month_to_month))
+    
+        #conditional to keep track of max and min profit change in variables
+        if profit_change > (max(month_to_month)):
+            greatest_month_increase = profit_change
+            increase_monthyear = row[0]
+
+        elif profit_change < (min(month_to_month)):
+            greatest_month_decrease = profit_change
+            decrease_monthyear = row[0]
+
+    #create variable for text file with printed results
+    financial_analysis = f"""
+    Financial Analysis
+    -----------------
+    Total Months: {count}
+    Total: ${net_total}
+    Average Change: {(round((sum(month_to_month)/len(month_to_month)),0))}
+    Greatest Increase in Profits: ${increase_monthyear} ({greatest_month_increase})
+    Greatest Decrease in Profits: ${decrease_monthyear} ({greatest_month_decrease})
+    """
+
+    #write new text file with results
+    print(financial_analysis)
+
+    with open("budget_data.txt", "w") as new_file:
+        new_file.write(financial_analysis)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,10 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
-#define empty lists for values below
-total_votes = []
-candidates = {"candidate_name": "votes"}
-candidates = dict()
-
+#define dictionaries for values below with total votes starting at zero
+votes = 0
+candidates = {}
+percentages = {}
 
 #read csv file
 with open(csvpath) as csvfile:
@@ -19,24 +18,39 @@ with open(csvpath) as csvfile:
     #read header row
     header = next(csvreader)
 
-    votes = 0
+    #loop through rows
     for row in csvreader:
         #count rows (votes)
         votes += 1
-        candidate_name = row[3]
-        #find candidate name and store in list
-    if candidate_name in candidates():
-        candidates["votes" +1]
-    else:
-        candidates[row[3]: 1]
-
-
+        #store candidate name in variable
+        candidate_name = row[2]
         
+        #find candidate name and store in dictionary or add to votes
+        if candidate_name in candidates.keys():
+            candidates[candidate_name] +=1
+            percentages[candidate_name] = candidates[candidate_name]/votes*100
+        else:
+            candidates[candidate_name] = 1
+            percentages[candidate_name] = candidates[candidate_name]/votes*100
+       
+        #find max key       
+        winner = max(candidates,key=candidates.get)
 
-print("Election Results")
-print("----------------")
-print("Total Votes: " + str(votes))
-print("----------------")
-# print(str(candidate1) + ":(" + str(sum(votes_candidate1)) + ")")
-# print(str(candidate2) + ":(" + str(sum(votes_candidate2)) + ")")
-# print(str(candidate3) + ":(" + str(sum(votes_candidate3)) + ")")
+#create variable for text file with printed results
+election_results = f"""
+Election Results
+----------------
+Total Votes: {votes}
+----------------
+Charles Casper Stockham: {round(percentages["Charles Casper Stockham"], 3)}% ({candidates["Charles Casper Stockham"]})
+Diana DeGette: {round(percentages["Diana DeGette"], 3)} % ({candidates["Diana DeGette"]})
+Raymon Anthony Doane: {round(percentages["Raymon Anthony Doane"], 3)} % ({candidates["Raymon Anthony Doane"]})
+Winner: {winner}
+----------------
+"""
+
+#write new text file with results
+print(election_results)
+
+with open("poll_results.txt", "w") as new_file:
+        new_file.write(election_results)
